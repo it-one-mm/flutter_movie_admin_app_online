fd253bce6dbd3db58f8b61e9b9fa6e550ed58afa
IT ONE MM <itone@gmail.com>
2020-09-25 15:40:01 +0630

    improve import

--- lib/screens/genre_form.dart
+++ lib/screens/genre_form.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_admin_app/services/genre_service.dart';
-import 'package:movie_admin_app/widgets/progress_hud.dart';
+import '../services/genre_service.dart';
+import '../widgets/progress_hud.dart';
 
 class GenreForm extends StatefulWidget {
   @override
